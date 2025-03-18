c78eba8f2e9369c32d63a0e146e19c85137f10f0
Oliver Pacut <opacut@redhat.com>
2025-03-18 09:33:54 +0100

    linting

--- src/Components/RegistrationProgress/FirstStep/FirstStep.cy.js
+++ src/Components/RegistrationProgress/FirstStep/FirstStep.cy.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import FirstStep from './FirstStep';
+//import React from 'react';
+//import FirstStep from './FirstStep';
 
 before(() => {
-    cy.mockWindowChrome();
+  cy.mockWindowChrome();
 });
 
 describe('Rendering tests.', () => {
-    // this is WIP
-    it('First step renders correctly.', () => {
-        cy.get('body');
-    })
-})
+  // this is WIP
+  it('First step renders correctly.', () => {
+    cy.get('body');
+  });
+});
