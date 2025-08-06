0c590f94b51040119c09b0c3c6278a1ebb9913d2
Michael Johnson <johnsonm325@gmail.com>
2025-08-06 08:36:00 -0400

    chore(RHINENG-19892): Remove old components (#230)
    * chore(RHINENG-19892): Remove old components
    
    This PR removes the feature flag for the registration assistant refresh feature, along with the old components.
    
    * chore: remove unused packages
    
    ---------
    
    Co-authored-by: Michael Johnson <micjohns@micjohns-thinkpadp16vgen1.rmtusnc.csb>

--- src/Routes/Register/Register.js
+++ src/Routes/Register/Register.js
@@ -1,215 +1,81 @@
-import './Register.scss';
-
+import React from 'react';
+import { Helmet } from 'react-helmet';
 import {
-  ColumnsIcon,
-  TasksIcon,
-} from '@patternfly/react-icons/dist/esm/icons/';
-import {
-  DataCollection,
-  EnablingInsightsOnRhui,
-  RegisterWithRhsm,
-  SetupConfigure,
-  Satellite,
-  SubscribetoSatellite,
-  schema,
-} from './Helpers';
-import {
-  Drawer,
-  DrawerActions,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerContentBody,
-  DrawerHead,
-  DrawerPanelBody,
-  DrawerPanelContent,
-} from '@patternfly/react-core/dist/esm/components/Drawer/';
-import {
+  Icon,
   PageSection,
   PageSectionVariants,
-} from '@patternfly/react-core/dist/esm/components/Page/';
-import {
-  FormTemplate as PfForm,
-  componentMapper,
-} from '@data-driven-forms/pf4-component-mapper';
-import React, { useState } from 'react';
-import {
   Text,
   TextContent,
-  TextVariants,
-} from '@patternfly/react-core/dist/esm/components/Text/index';
-
-import { Button } from '@patternfly/react-core/dist/esm/components/Button/index';
-import { Divider } from '@patternfly/react-core/dist/esm/components/Divider/index';
-import FormRenderer from '@data-driven-forms/react-form-renderer/form-renderer/form-renderer';
-import FormSpy from '@data-driven-forms/react-form-renderer/form-spy/form-spy';
-import Group from '../../Components/Group/Group';
-import { Helmet } from 'react-helmet';
-import PropTypes from 'prop-types';
-import { Title } from '@patternfly/react-core/dist/esm/components/Title/index';
-import messages from '../../Messages';
-import { useIntl } from 'react-intl';
-import { Icon } from '@patternfly/react-core';
-
-const CustomSection = ({ label }) => <React.Fragment>{label}</React.Fragment>;
-
-CustomSection.propTypes = {
-  label: PropTypes.any,
-};
-
-const insRaMapper = {
-  ...componentMapper,
-  'custom-section': { component: CustomSection },
-};
+} from '@patternfly/react-core';
+import {
+  dataCollection,
+  dataCollectionLink,
+  insightsUsingProxy,
+  insightsUsingProxyLink,
+  insightsUsingSatellite,
+  insightsUsingSatelliteLink,
+  regAssistantDescription,
+} from '../../constants';
+import {
+  PageHeader,
+  PageHeaderTitle,
+} from '@redhat-cloud-services/frontend-components/PageHeader';
+import RegProgessStepper from '../../Components/RegistrationProgress/RegProgressStepper';
+import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 
 const Register = () => {
-  const intl = useIntl();
-
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  const onOpenClick = () => {
-    setIsExpanded(true);
-  };
-
-  const onCloseClick = () => {
-    setIsExpanded(false);
-  };
-
-  const panelContent = (
-    <DrawerPanelContent className="ins-m-light-300" hasNoBorder noPadding>
-      <ul aria-label="Red Hat Insights tips">
-        <FormSpy>
-          {({ values }) =>
-            values['rhel-os'] === 'rhel84+' ? (
-              ''
-            ) : (
-              <React.Fragment>
-                <li>
-                  <DrawerHead>
-                    <Group type="title-group">
-                      <Icon size="lg">
-                        <TasksIcon />
-                      </Icon>
-                      <Title headingLevel="h3" size="md">
-                        {intl.formatMessage(messages.preinstallationChecks)}
-                      </Title>
-                    </Group>
-                    {isExpanded && (
-                      <DrawerActions>
-                        <DrawerCloseButton onClick={onCloseClick} />
-                      </DrawerActions>
-                    )}
-                  </DrawerHead>
-                  <DrawerPanelBody>
-                    <FormSpy>
-                      {({ values }) =>
-                        values['how-are-systems-managed'] === 'rhsm' ? (
-                          <RegisterWithRhsm intl={intl} />
-                        ) : values['how-are-systems-managed'] === 'rhs' ? (
-                          <SubscribetoSatellite intl={intl} />
-                        ) : values['how-are-systems-managed'] === 'rhui' ? (
-                          <EnablingInsightsOnRhui intl={intl} />
-                        ) : (
-                          intl.formatMessage(messages.pleaseIndicate)
-                        )
-                      }
-                    </FormSpy>
-                  </DrawerPanelBody>
-                </li>
-                <Divider component="li" />
-              </React.Fragment>
-            )
-          }
-        </FormSpy>
-        <li>
-          <DrawerPanelBody>
-            <DataCollection intl={intl} />
-          </DrawerPanelBody>
-        </li>
-        <Divider component="li" />
-        <li>
-          <DrawerPanelBody>
-            <SetupConfigure intl={intl} />
-          </DrawerPanelBody>
-        </li>
-        <Divider component="li" />
-        <li>
-          <FormSpy>
-            {({ values }) =>
-              values['how-are-systems-managed'] !== 'rhs' ? (
-                <DrawerPanelBody>
-                  <Satellite intl={intl} />
-                </DrawerPanelBody>
-              ) : null
-            }
-          </FormSpy>
-        </li>
-        <Divider component="li" />
-        <li aria-hidden="true">
-          <DrawerPanelBody></DrawerPanelBody>
-        </li>
-      </ul>
-    </DrawerPanelContent>
-  );
-
-  const FormTemplate = (props) => (
-    <PageSection
-      className="ins-c-registration-assistant ins-c-overflow-content"
-      variant={PageSectionVariants.light}
-      padding={{ default: 'noPadding' }}
-    >
-      <Drawer
-        className="ins-c-registration-assistant-drawer"
-        isStatic
-        isExpanded={isExpanded}
-      >
-        <DrawerContent panelContent={panelContent}>
-          <DrawerContentBody hasPadding>
-            <Group type="page-title">
-              <TextContent>
-                <div className="ins-c-page-title__main">
-                  <Title headingLevel="h1" size="xl">
-                    {intl.formatMessage(messages.registerYourSystems)}
-                  </Title>
-                  <Button aria-expanded={isExpanded} onClick={onOpenClick}>
-                    <ColumnsIcon />
-                  </Button>
-                </div>
-                <Text component={TextVariants.small}>
-                  {intl.formatMessage(messages.insightsRegistrationAssistant)}
-                </Text>
-                <Title className="ins-c-step-title" headingLevel="h2" size="md">
-                  {intl.formatMessage(messages.stepOneTitle)}
-                </Title>
-              </TextContent>
-            </Group>
-            <div className="ins-c-registration-assistant-form">
-              <PfForm {...props} />
-            </div>
-          </DrawerContentBody>
-        </DrawerContent>
-      </Drawer>
-    </PageSection>
-  );
-
   return (
     <React.Fragment>
       <Helmet>
         <meta charSet="utf-8" />
         <title>Register Systems | RHEL</title>
       </Helmet>
-      <FormRenderer
-        schema={schema(intl)}
-        componentMapper={insRaMapper}
-        FormTemplate={(props) => (
-          <FormTemplate {...props} showFormControls={false} />
-        )}
-      />
+      <PageHeader>
+        <PageHeaderTitle
+          style={{ paddingBottom: '16px' }}
+          title="Registration Assistant"
+        />
+        <TextContent>
+          <Text>{regAssistantDescription}</Text>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href={insightsUsingProxyLink}
+          >
+            {insightsUsingProxy}
+            <Icon className="pf-v5-u-ml-xs">
+              <ExternalLinkAltIcon />
+            </Icon>
+          </a>
+          <br />
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href={insightsUsingSatelliteLink}
+          >
+            {insightsUsingSatellite}
+            <Icon className="pf-v5-u-ml-xs">
+              <ExternalLinkAltIcon />
+            </Icon>
+          </a>
+          <br />
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href={dataCollectionLink}
+          >
+            {dataCollection}
+            <Icon className="pf-v5-u-ml-xs">
+              <ExternalLinkAltIcon />
+            </Icon>
+          </a>
+        </TextContent>
+      </PageHeader>
+      <PageSection variant={PageSectionVariants.light}>
+        <RegProgessStepper />
+      </PageSection>
     </React.Fragment>
   );
-};
-
-Register.propTypes = {
-  formFields: PropTypes.object,
 };
 
 export default Register;
